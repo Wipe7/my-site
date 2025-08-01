2745944076c9efc1a86c7a61b4d79b2cef09fc7e
Wipe7 <wpeixototeixeira@gmail.com>
2025-08-01 19:06:51 -0300

    Adicionando arquivos funcionais django-admin

--- EBAC/models.py
+++ EBAC/models.py
@@ -1,11 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+STATUS_CHOICES = (
+    ('draft', 'Draft'),
+    ('published', 'Published'),
+)   
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    slug = models.SlugField(unique=True, default='sem-slug')
+    status = models.CharField(max_length=10, choices= STATUS_CHOICES, default='draft')
+    created_on = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
