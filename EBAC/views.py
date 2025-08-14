e18ff19f6ad6b5a05e93fbaba2252e12c4332d09
Wipe7 <wpeixototeixeira@gmail.com>
2025-08-14 19:03:28 -0300

    Arquivos modificados

--- EBAC/views.py
+++ EBAC/views.py
@@ -1,4 +1,10 @@
 from django.views.generic import TemplateView
+from .models import Post
+from django.shortcuts import render
 
 class PostView(TemplateView):
     template_name = 'index.html'
+
+def list_posts(request):
+    posts = Post.objects.filter(status='published').order_by('-created_on')
+    return render(request, 'post_detail.html', {'posts': posts})
